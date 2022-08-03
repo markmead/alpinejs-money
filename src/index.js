49271662ef0259f8f1bf054c2b06ec0aa9f5b34d
Mark Mead <itsmarkmead@gmail.com>
2022-08-03 15:51:07 +0100

    Init

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 export default function (Alpine) {
   Alpine.directive(
-    "money",
+    'money',
     (el, { expression, modifiers }, { evaluateLater, effect }) => {
-      let { lang, currency } = el.dataset;
-      let isShopify = modifiers.includes("shopify");
-      let formatLang = isShopify ? Shopify.locale : lang;
-      let formatCurrency = isShopify ? Shopify.currency.active : currency;
-      let getValue = evaluateLater(expression);
+      let { lang, currency } = el.dataset
+      let isShopify = modifiers.includes('shopify')
+      let formatLang = isShopify ? Shopify.locale : lang
+      let formatCurrency = isShopify ? Shopify.currency.active : currency
+      let getValue = evaluateLater(expression)
 
       effect(() => {
         getValue((value) => {
-          let money = value / 100;
+          let money = value / 100
           let formattedPrice = new Intl.NumberFormat(formatLang, {
-            style: "currency",
+            style: 'currency',
             currency: formatCurrency,
-          }).format(money);
+          }).format(money)
 
-          el.innerText = formattedPrice;
-        });
-      });
+          el.innerText = formattedPrice
+        })
+      })
     }
-  );
+  )
 }
