7f6f214166654d79a3893015a4e778ebfc8bf849
Mark Mead <50486078+markmead@users.noreply.github.com>
2022-12-09 09:39:37 +0000

    Update - ES Build 0.16 (#4)

--- builds/cdn.js
+++ builds/cdn.js
@@ -1,5 +1,3 @@
 import money from '../src/index.js'
 
-document.addEventListener('alpine:init', () => {
-  window.Alpine.plugin(money)
-})
+document.addEventListener('alpine:init', () => window.Alpine.plugin(money))
